03c924fefe495f33e0faf4570c3009515d8d3849
Michael Cho <michael.cho@mail.com>
2023-10-16 11:50:25 +1100

    optional missing files

--- src/flask_vite/tags.py
+++ src/flask_vite/tags.py
@@ -14,25 +14,33 @@ def make_tag(static: bool = False, entry_point: Optional[str] = None):
 
 
 def make_static_tag(entry_point: Optional[str]):
+    js_file = css_file = None
     js_files = glob.glob("vite/dist/assets/*.js")
     css_files = glob.glob("vite/dist/assets/*.css")
 
-    js_file = js_files[0]
-    css_file = css_files[0]
-    if entry_point:
-        js_file = next((f for f in js_files if _get_filename(f).startswith(entry_point)), None)
-        css_file = next((f for f in css_files if _get_filename(f).startswith(entry_point)), None)
+    if css_files:
+        css_file = css_files[0]
+        if entry_point:
+            css_file = next((f for f in css_files if _get_filename(f).startswith(entry_point)), None)
 
-        if not js_file or not css_file:
-            raise FileNotFoundError(f"No file was found for entrypoint: {entry_point}")
+    if js_files:
+        js_file = js_files[0]
 
-    return dedent(
-        f"""
-            <!-- FLASK_VITE_HEADER -->
-            <script type="module" src="/_vite/{_get_filename(js_file)}"></script>
-            <link rel="stylesheet" href="/_vite/{_get_filename(css_file)}"></link>
-        """
-    ).strip()
+        if entry_point:
+            js_file = next((f for f in js_files if _get_filename(f).startswith(entry_point)), None)
+
+    tags = ""
+    if js_file:
+        tags += dedent(
+            f"""
+                <script type="module" src="/_vite/{_get_filename(js_file)}"></script>
+            """
+        )
+
+    if css_file:
+        tags += f'<link rel="stylesheet" href="/_vite/{_get_filename(css_file)}"></link>'
+
+    return tags.strip()
 
 
 def make_debug_tag(entry_point: Optional[str]):
